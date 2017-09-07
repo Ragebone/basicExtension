4ff948850b5a90d9b962e6e40ea0e450d6811209
Norman Göhl <normangoehl@hotmail.de>
2017-09-07 16:39:51 +0200

    Add Actual Code
    Remove class1.cs file because i fucked up that name!
    Add extension.cs .
    Add the actual Code to exttension.cs ; Code comes from another private repo.

---
+++ basicExtension/extension.cs
@@ -0,0 +1,54 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using RGiesecke.DllExport;
+using System.Collections.Generic;
+
+namespace basicExtension
+{
+    public class Extension
+    {
+        private static StringBuilder outP;
+        // Query dictionary.
+        private delegate int Del(string[] a);
+        private static readonly Dictionary<string, Del> querys = new Dictionary<string, Del> {
+            {"test", test}
+        };
+
+        [DllExport("RVExtensionVersion", CallingConvention = CallingConvention.Winapi)]
+        public static void RVExtensionVersion(StringBuilder output, int outputSize)
+        {
+            outputSize--;
+            output.Append("R1");
+        }
+
+        [DllExport("RVExtensionArgs", CallingConvention = CallingConvention.Winapi)]
+        public static int RVExtensionArgs(StringBuilder output, int outputSize, string function, string[] args, int argsCnt)
+        {
+            outputSize--;
+            outP = output;
+            output.Append("");
+
+            Del method;
+            if (querys.TryGetValue(function.ToLower(), out method))
+            {
+                return method(args);
+            }
+            return 0;
+        }
+
+        // works
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+        public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string id)
+        {
+            outputSize--;
+            output.Append("not supported");
+        }
+
+        // Query Methods :
+        private static int test(string[] id)
+        {
+            outP.Append("true");
+            return 0;
+        }
+    }
+}
